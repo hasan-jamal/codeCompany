99ea55e5bf3da5ae0665df452dcc8764186b1c76
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2025-07-06 11:42:01 +0300

    Update

--- src/app/pages/our-servcies/components/servcies/servcies.component.ts
+++ src/app/pages/our-servcies/components/servcies/servcies.component.ts
@@ -1,10 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ModalService } from '../../../../services/ModalService';
+import { OurServiceFlowComponent } from '../../../../shared/sectionsPublic/our-service-flow/our-service-flow.component';
+import { ServicesS4Component } from '../../../../shared/sectionsPublic/services-s4/services-s4.component';
 
 @Component({
   selector: 'app-servcies',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, OurServiceFlowComponent, ServicesS4Component],
   templateUrl: './servcies.component.html',
   styleUrls: ['./servcies.component.css',
                   '../../../../../assets/css/pages/ourServcies.css',
